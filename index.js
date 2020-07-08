39060c70d89eb826aa7cf3bc242dafb5c5ffa3d1
Charise Arter <mine@Charises-MacBook-Pro.local>
2020-07-08 14:33:05 -0500

    users and posts server setup

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 // code away!
 const express = require('express');
 
-//router for posts
+const server = express();
+server.use(express.json());
+//router setUp
 //const routerName = require('./routerFolder/routerName');
 //server end point base
 //server.use('/api/posts', routerName);
 
-const server = express();
-server.use(express.json());
+//Posts Router
+const postRouter = require('./posts/postRouter');
+server.use('/api/posts', postRouter);
+//Users Router
+const userRouter = require('./users/userRouter'); 
+server.use('/api/users', userRouter);
+
 
 const PORT = 8000;
 server.listen(PORT, () => {
