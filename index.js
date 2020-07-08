226a87130a13ba93e5e1599d3a0e5c285134cf92
Charise Arter <mine@Charises-MacBook-Pro.local>
2020-07-08 14:25:35 -0500

    initial setup of server

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code away!
+const express = require('express');
+
+//router for posts
+//const routerName = require('./routerFolder/routerName');
+//server end point base
+//server.use('/api/posts', routerName);
+
+const server = express();
+server.use(express.json());
+
+const PORT = 8000;
+server.listen(PORT, () => {
+  console.log('<h2> Server for Node Project 3</h2>');
+});
